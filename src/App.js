189b8708d788a792eedce903fa7aef1ef29712f9
CoderCarti <bladeruneking@gmail.com>
2025-07-24 18:42:30 +0800

    ADJUST: remove login from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Homepage from './Components/Homepage';
 import Navbar from './Components/Navbar';
-import Login from './Components/Login';
+
 
 function App() {
   return (
@@ -9,7 +9,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
       </Router>
   );
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -89,7 +89,6 @@ const Homepage = () => {
             ))}
           </div>
         </section>
-        hello
       </div>
 
       {/* Projects Section */}
